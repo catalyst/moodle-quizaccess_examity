f541365e5e14a5213b06131acf140a91c2c430ef
Antony Videtta <antonyvidetta@catalyst.net.nz>
2021-06-16 11:11:33 +1200

    WR355129 - shift examity custom role into helper class

--- classes/helper.php
+++ classes/helper.php
@@ -747,5 +747,31 @@ class helper {
 
                 return $lti;
     }
+
+    /**
+     * get custom examity role
+     *
+     * @param string $table.
+     * @param string $column.
+     * @param string $value.
+     * @return object $id.
+     */
+    public static function get_examity_role() {
+
+        global $DB;
+        create_role('Examity', 'examity', 'Gives access to examity API functions');
+        $rolerecord = $DB->get_record('role', array("shortname" => 'examity'), $fields = '*');
+        set_role_contextlevels($rolerecord->id, array(CONTEXT_SYSTEM));
+
+        $context = context_system::instance();
+        assign_capability('quizzaccessrule/examity:get_course_contents', CAP_ALLOW, $rolerecord->id, $context->id, true);
+        assign_capability('quizzaccessrule/examity:get_enrolled_user', CAP_ALLOW, $rolerecord->id, $context->id, true);
+        assign_capability('quizzaccessrule/examity:get_quiz_by_course', CAP_ALLOW, $rolerecord->id, $context->id, true);
+        assign_capability('quizzaccessrule/examity:validate_parameters', CAP_ALLOW, $rolerecord->id, $context->id, true);
+
+        $context->mark_dirty();
+    } 
+
+
 }
 
