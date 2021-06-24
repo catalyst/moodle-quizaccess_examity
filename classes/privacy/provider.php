c38731d8764fa4c15713935fe4ff2a0351b0a450
Antony Videtta <antonyvidetta@catalyst.net.nz>
2021-06-24 12:45:08 +1200

    WR355129 - add privacy class

---
+++ classes/privacy/provider.php
@@ -0,0 +1,107 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Privacy Subsystem implementation for quizaccess_examity
+ *
+ * @package   quizaccess_examity
+ * @author    Ant
+ * @copyright 2021 Ant
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace quizaccess_examity\privacy;
+
+defined('MOODLE_INTERNAL') || die();
+use core_privacy\local\metadata\collection;
+use core_privacy\local\request\{writer, helper, contextlist, approved_contextlist, approved_userlist, userlist};
+
+/**
+ * Privacy subsystem for quizaccess_examity.
+ *
+ * @copyright 2021 Ant
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements
+    \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\core_userlist_provider {
+
+    /**
+     * Return the fields which contain personal data.
+     *
+     * @param collection $collection a reference to the collection to use to store the metadata.
+     * @return collection the updated collection of metadata items.
+     */
+    public static function get_metadata(collection $collection) : collection {
+
+        $collection->add_database_table(
+            'quizaccess_examity_u',
+            [
+                'userid' => 'privacy:metadata:quizaccess_examity_u:userid',
+                'examity_user_id' => 'privacy:metadata:quizaccess_examity_u:examity_user_id',
+            ], 'privacy:metadata:quizaccess_examity_u'
+        );
+
+        return $collection;
+    }
+    /**
+     * Get the list of contexts that contain user information for the specified user.
+     *
+     * @param int $userid the userid.
+     * @return contextlist the list of contexts containing user info for the user.
+     */
+    public static function get_contexts_for_userid($userid) : contextlist {
+
+    }
+    /**
+     * Export all examity data for the specified userid and context.
+     *
+     * @param   int         $userid The user to export.
+     * @param   \context    $context The context to export.
+     * @param   array       $subcontext The subcontext within the context to export this information to.
+     * @param   array       $linkarray The weird and wonderful link array used to display information for a specific item
+     */
+    public static function export_examity_user_data(int $userid, \context $context, array $subcontext, array $linkarray) {
+
+    }
+
+    /**
+     * Delete all user information for the provided user and context.
+     *
+     * @param  int      $userid    The user to delete
+     * @param  \context $context   The context to refine the deletion.
+     */
+    public static function delete_examity_for_user(int $userid) {
+
+    }
+
+    /**
+     * Get the list of users who have data within a context.
+     *
+     * @param   userlist    $userlist   The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist) {
+
+    }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param   approved_userlist       $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist) {
+
+    }
+}
--- lang/en/quizaccess_examity.php
+++ lang/en/quizaccess_examity.php
@@ -69,3 +69,8 @@ $string['disable'] = 'Disable';
 $string['enable'] = 'Enable';
 $string['logintoexamity'] = 'Click here to login into Examity';
 $string['web_services'] = 'Examity web services';
+
+$string['privacy:metadata:quizaccess_examity_u:userid'] = 'The Moodle userid';
+$string['privacy:metadata:quizaccess_examity_u:examity_user_id'] = 'The user id which the examity system uses';
+$string['privacy:metadata:quizaccess_examity_u'] = 'Stores details about the user';
+
