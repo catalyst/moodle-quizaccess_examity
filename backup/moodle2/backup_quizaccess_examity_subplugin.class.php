895ed10e3c63d0d59a66f405423f94cd89ff4c6b
Antony Videtta <antonyvidetta@catalyst.net.nz>
2021-06-25 16:36:37 +1200

    WR355129 - add backup script for custom examity tables

---
+++ backup/moodle2/backup_quizaccess_examity_subplugin.class.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Contains class backup_quizaccess_examity_subplugin
+ *
+ * @package   quizaccess_examity
+ * @copyright 2021 Ant
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+require_once($CFG->dirroot . '/mod/quiz/backup/moodle2/backup_mod_quiz_access_subplugin.class.php');
+
+/**
+ * Class backup_quizaccess_examity_subplugin
+ *
+ * @package   quizaccess_examity
+ * @copyright 2021 Ant
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class backup_quizaccess_examity_subplugin extends backup_mod_quiz_access_subplugin {
+    /**
+     * Stores the data related to the examity quiz access rule settings for a particular quiz.
+     *
+     * @return backup_subplugin_element
+     */
+    protected function define_quiz_subplugin_structure() {
+
+        // Create XML elements.
+        $subplugin = $this->get_subplugin_element();
+        $subpluginwrapper = new backup_nested_element($this->get_recommended_name());
+        $subplugintablesettings = new backup_nested_element('quizaccess_examity_u',
+                null, array('id', 'userid', 'examity_user_id'));
+
+        // Connect XML elements into the tree.
+        $subplugin->add_child($subpluginwrapper);
+        $subpluginwrapper->add_child($subplugintablesettings);
+
+        // Set source to populate the data.
+        $subplugintablesettings->set_source_table('quizaccess_examity_u',
+        array('quizid' => backup::VAR_ACTIVITYID));
+
+        return $subplugin;
+    }
+}
