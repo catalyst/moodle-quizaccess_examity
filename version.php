b1c4f97ab4480ebc66e37466033591e291cf8c96
Dan Marsden <dan@danmarsden.com>
2021-09-24 10:24:11 +1200

    Bump version.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_examity';
-$plugin->release = '0.1.0';
-$plugin->version = 2021062800;
+$plugin->release = '0.1.1';
+$plugin->version = 2021092400;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
